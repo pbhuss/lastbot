fc130535fe1d46a21ee8bce255b3c33a5b74e71f
Peter Huss <pbhuss@gmail.com>
2018-05-20 19:15:04 -0700

    Updated webpage. /unlink command. Authentication for users page

--- lastbot.py
+++ lastbot.py
@@ -1,5 +1,7 @@
 import sqlite3
 import time
+from functools import wraps
+
 import pylast
 import yaml
 from datetime import datetime
@@ -13,10 +15,11 @@ from flask import Response
 from pylast import LastFMNetwork
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 def get_db():
@@ -63,6 +66,50 @@ def get_verification_token():
         g.slack_verification_token = api_config['slack_verification_token']
 
     return g.slack_verification_token
+
+
+def get_admin_password():
+    if 'admin_password' not in g:
+        with open('api.yaml', 'r') as fp:
+            api_config = yaml.load(fp)
+        g.admin_password = api_config['admin_password']
+    return g.admin_password
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == get_admin_password()
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials',
+        status=401,
+        headers={'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
+def requires_token(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if request.form.get('token') != get_verification_token():
+            return Response(status=400)
+        return f(*args, **kwargs)
+    return decorated
 
 
 def quote_user_id(user_id):
@@ -144,9 +191,8 @@ def build_attachment(track, start_time):
 
 
 @app.route('/playing', methods=['POST'])
+@requires_token
 def playing():
-    if request.form.get('token') != get_verification_token():
-        return Response(status=400)
     start_time = time.time()
     form_text = request.form.get('text')
     if form_text:
@@ -194,9 +240,8 @@ def playing():
 
 
 @app.route('/userinfo', methods=['POST'])
+@requires_token
 def user_info():
-    if request.form.get('token') != get_verification_token():
-        return Response(status=400)
     start_time = time.time()
     form_text = request.form.get('text')
     if form_text:
@@ -284,9 +329,8 @@ def user_info():
 
 
 @app.route('/register', methods=['POST'])
+@requires_token
 def register():
-    if request.form.get('token') != get_verification_token():
-        return Response(status=400)
     team_id = request.form.get('team_id')
     user_id = request.form.get('user_id')
     lastfm_user = request.form.get('text')
@@ -333,13 +377,52 @@ def register():
             lastfm_user, quote_user_id(user_id))
 
 
+@app.route('/unlink', methods=['POST'])
+@requires_token
+def unlink():
+    team_id = request.form.get('team_id')
+    user_id = request.form.get('user_id')
+    if not all((team_id, user_id)):
+        return "Missing required field"
+
+    db = get_db()
+    c = db.cursor()
+
+    row = c.execute(
+        "SELECT id, lastfm_user FROM users WHERE team_id = ? AND user_id = ?",
+        (team_id, user_id,)
+    ).fetchone()
+    if row:
+        id_, lastfm_user, = row
+        c.execute(
+            "DELETE FROM users WHERE id = ?",
+            (id_,)
+        )
+        db.commit()
+        c.close()
+        return "Unlinked LastFM user `{}` from {}".format(
+            lastfm_user, quote_user_id(user_id))
+    else:
+        c.close()
+        return get_response(
+            ResponseType.EPHEMERAL,
+            'No account registered to {}'.format(quote_user_id(user_id))
+        )
+
+
 @app.route('/users')
+@requires_auth
 def users():
     c = get_db().cursor()
     rows = c.execute("SELECT * FROM users;").fetchall()
     c.close()
 
     return render_template('users.html', users=rows)
+
+
+@app.route('/commands')
+def commands():
+    return render_template('commands.html')
 
 
 if __name__ == '__main__':
